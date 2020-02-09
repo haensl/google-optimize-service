5c1524485d98c7fcd8ea4378c35bc04812ff978c
HP Dietz <hp@jandh.org>
2020-02-09 20:16:10 +0100

    Add test for node.js package.

---
+++ src/package-tests/node.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const optimize = require('google-optimize-service');
+const store = {};
+
+optimize.configure({
+  key: 'test-app',
+  location: {
+    search: '?variant=tooltip&utm_expid=1'
+  },
+  storage: {
+    setItem: (key, value) => store[key] = value,
+    getItem: (key) => store[key]
+  }
+});
+
+const experiment = optimize.get();
+console.info(`Node.js v${process.versions.node}`);
+console.info('experiment', experiment);
+assert(
+  'experimentId' in experiment,
+  'experiment does not contain experimentId!'
+);
+assert(
+  'variant' in experiment,
+  'experiment does not contain variant!'
+);
+assert.equal(
+  experiment.experimentId,
+  '1',
+  `experimentId does not match! Expected: '1'. Actual: ${experiment.experimentId}`
+);
+assert.equal(
+  experiment.variant,
+  'tooltip',
+  `variant does not match! Expected: 'tooltip'. Actual: ${experiment.variant}`
+);
